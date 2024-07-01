19712148956dd7d5ad3efa2eb80b86602921b2b9
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-01 18:01:40 -0300

    classe Arma

---
+++ src/Arma.java
@@ -0,0 +1,49 @@
+public class Arma {
+    private String nome;
+    private int municao;
+    private String modos;
+    private boolean miraIntegrada;
+
+    public void somArma() {
+        System.out.println("Pow, pow");
+    }
+
+    public Arma(String nome, int municao, String modos, boolean miraIntegrada) {
+        this.nome = nome;
+        this.municao = municao;
+        this.modos = modos;
+        this.miraIntegrada = miraIntegrada;
+    }
+
+    /*public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setMunicao(int municao) {
+        this.municao = municao;
+    }
+
+    public void setModos(String modos) {
+        this.modos = modos;
+    }
+
+    public void setMiraIntegrada(boolean miraIntegrada) {
+        this.miraIntegrada = miraIntegrada;
+    }*/
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getMunicao() {
+        return municao;
+    }
+
+    public String getModos() {
+        return modos;
+    }
+
+    public boolean isMiraIntegrada() {
+        return miraIntegrada;
+    }
+}
