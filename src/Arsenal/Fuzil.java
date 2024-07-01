61aa83f6c2def8686a63dc1fa3c92e6ccd8ba930
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-01 18:35:38 -0300

    subclasse objeto

--- src/Arsenal/Fuzil.java
+++ src/Arsenal/Fuzil.java
@@ -2,4 +2,18 @@ package Arsenal;
 
 public class Fuzil extends Arma {
 
+    public static void main(String[] args) {
+        Arma minhaArma = new Arma();
+
+        minhaArma.setNome("Ak47");
+        minhaArma.setModos("Automático, Semi");
+        minhaArma.setMunicao(30);
+        minhaArma.setMiraIntegrada(true);
+
+        System.out.println(minhaArma.getNome());
+        System.out.println(minhaArma.getModos());
+        System.out.println(minhaArma.getMunicao());
+        System.out.println(minhaArma.isMiraIntegrada());
+
+    }
 }
