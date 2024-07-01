2db763835b1f020bda607d877578851bad7fb139
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-01 18:58:24 -0300

    correção na instanciação de objetos

--- src/Arsenal/Fuzil.java
+++ src/Arsenal/Fuzil.java
@@ -1,13 +1,23 @@
 package Arsenal;
 
 public class Fuzil extends Arma {
-    public Fuzil(String nome, int municao, String modos, boolean miraInegrada) {
-        super(nome, municao, modos, miraInegrada);
+    public Fuzil(String nome, int municao, String modos, boolean miraIntegrada) {
+        super(nome, municao, modos, miraIntegrada);
+    }
+
+    @Override
+    public void somArma() {
+        super.somArma();
+        System.out.println("Ta, ra, ta, ta, ta");
     }
 
     public static void main(String[] args) {
-        Arma fuzil01 = new Arma("Ak47", 30, "Travado, automático, semiautomático", true);
+        Fuzil fuzil01 = new Fuzil("Ak47", 30, "Travado, automático, semiautomático", true);
+        Fuzil fuzil02 = new Fuzil("M4", 30, "Travado, semiautomático, rajada", true);
 
         fuzil01.fichaTecnica();
+        fuzil02.fichaTecnica();
+
+        fuzil01.somArma();
     }
 }
